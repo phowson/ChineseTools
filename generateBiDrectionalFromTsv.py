a9735a50bca6e1707be9dfdcbafd46a2919e5aec
phowson <phil@howson.net>
2017-09-07 12:36:48 +0100

    Adding new reverse deck generation

---
+++ generateBiDrectionalFromTsv.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+import requests
+import sys;
+from sets import Set
+from MandarinDefTools import *;
+import pinyin;
+from gtts import gTTS;
+import os;
+import csv;
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+sDir = sys.argv[2] +'/';
+
+sDir1 = sDir +'/MandEng/';
+sDir2 = sDir +'/EngMand/';
+
+
+
+outputFolderNames=[];
+
+outputFolderNames.append(sDir1)
+outputFolderNames.append(sDir2)
+
+for d in outputFolderNames:
+	if not os.path.exists(d):
+		os.makedirs(d)
+
+ax=0;
+f = open(sDir1+'/deck.tsv', 'w');
+f2 = open(sDir2 +'/deck.tsv', 'w');
+with open(sys.argv[1], 'rb') as csvfile:
+	creader = csv.reader(csvfile, delimiter='\t')
+
+	for row in creader:
+		mandarin = row[0];
+		pinyin = row[1];
+		definition = row[2];
+		tag = "SECTION" + str(ax/10);
+
+		tts = gTTS(text=mandarin, lang='zh-cn')
+		filename = 'sentaudio' + str(ax) +'.mp3';
+		if (os.path.isfile(sDir1+filename)):
+			os.remove(sDir1+filename);
+
+		if (os.path.isfile(sDir2+filename)):
+			os.remove(sDir2+filename);
+
+		tts.save(sDir1+filename);
+		tts.save(sDir2+filename);
+
+		f.write(mandarin +'\t' + pinyin+ '<br/><p>' + definition +'</p>\t'+tag+'\t\t\t'+filename+'\t\n');
+		f.flush();
+
+		f2.write(definition +'\t' + pinyin+ '<br/><p>' + mandarin +'</p>\t'+tag+'\t\t\t\t'+filename+'\n');
+		f2.flush();
+
+		ax=ax+1;
+		print(str(ax));
+
+f.close();
+f2.close();
+
+
+print "compressing"
+i=0;
+for d in outputFolderNames:
+	with zipfile.ZipFile(sys.argv[2] +"_"+ str(i)+'.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+		zipdir(d, zipf);
+		i=i+1
---
+++ hsk4extract2.py
@@ -0,0 +1,161 @@
+
+import requests
+from MandarinDefTools import *;
+import sys;
+from gtts import gTTS;
+import urllib;
+
+
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+
+
+DIVISOR=50;
+FOLDERS=13
+includeExamples=False;
+
+
+response = requests.get('http://data.hskhsk.com/lists/HSK%20Official%20With%20Definitions%202012%20L4%20freqorder.txt');
+
+lines = response.content.split('\n');
+
+
+outputFiles=[];
+outputFilesRev=[];
+outputFolderNames=[];
+outputFolderNamesRev=[];
+
+dirPrefix='HSK4Deck2';
+dirPrefix2='HSK4Deck2Rev';
+
+for i in range(0,FOLDERS):
+	directory = dirPrefix + str(i);
+	directory2 = dirPrefix + 'rev' + str(i);
+	
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
+	if not os.path.exists(directory2):
+		os.makedirs(directory2)
+
+	f = open(directory+'/deck.tsv', 'w');
+	outputFiles.append(f);
+	outputFolderNames.append(directory);
+
+
+	f2 = open(directory2+'/deck.tsv', 'w');
+	outputFilesRev.append(f2);
+	outputFolderNamesRev.append(directory2);
+
+
+counter = 0;
+for row in lines:
+	cols = row.split('\t');
+	
+
+
+
+	if (len(cols)<4):
+		break;
+	col1=cols[0];
+	col2=cols[1];
+	col4=cols[3];
+	col5=cols[4];
+
+
+
+	char = col1.strip();
+
+	x = counter/DIVISOR;
+
+	sDir = outputFolderNames[x];
+	sDir2 = outputFolderNamesRev[x];
+	#knownWord = wordMap.get(char);
+	filename= str(counter) +'.mp3';
+	if (os.path.isfile(sDir+'/'+filename)):
+		os.remove(sDir+'/'+filename);
+
+	tts = gTTS(text=char, lang='zh-cn')
+	tts.save(sDir+'/'+filename)		
+	tts.save(sDir2+'/'+filename)		
+
+
+	if includeExamples:
+		examples = getExamples(char);
+	else:
+		examples='';
+
+	tag = "SECTION" + str(counter / 10);
+
+	txt= '<font size=+3>';
+	txt+= char;
+	txt+= '</font>\t';
+	txt+= reduceLen(clean(col4));
+	txt+= '<br/>';
+	txt+= reduceLen(clean(col5));
+	txt+= '<br/>';
+	txt+= getDefinition(char);
+	txt+= '<br/>';
+	txt+= examples
+	txt+= '<br/>';
+	txt+= 'Rank ' + str(counter+1);
+	txt+= '\t'+tag+'\t\t\t'+filename+'\t\n'
+
+
+	ofls = outputFiles[x];
+	ofls.write(txt);
+	ofls.flush();
+
+
+
+	txt2= getDefinition(char);	
+	txt2+= '\t';
+	txt2+= char;
+	txt2+= '<br/>';
+	txt2+= reduceLen(clean(col4));
+	txt2+= '<br/>';
+	txt2+= reduceLen(clean(col5));
+	txt2+= '<br/>';
+	txt2+= examples;
+	txt2+= '<br/>';
+	txt2+= 'Rank ' + str(counter+1);
+	txt2+= '\t'+tag+'\t\t\t'+filename+'\t\n'
+
+
+	ofls2 = outputFilesRev[x];
+	ofls2.write(txt2);
+	ofls2.flush();
+
+
+
+
+	counter = counter+1;
+	print counter;
+
+
+
+
+for f in outputFiles:	
+	f.close();
+
+for f in outputFilesRev:	
+	f.close();
+
+
+print "compressing"
+i=0;
+
+for d in outputFolderNamesRev:
+	with zipfile.ZipFile('HSK4 EngZh deck' + str(i)+'.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+		zipdir(d, zipf);
+
+		i=i+1
+
+for d in outputFolderNames:
+	with zipfile.ZipFile('HSK4 ZhEng deck' + str(i)+'.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+		zipdir(d, zipf);
+
+		i=i+1
